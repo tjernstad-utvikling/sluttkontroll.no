d37ad13b2fe5c37a664ae34450f17367463dcfe5
Ole Tjernstad <ott556@gmail.com>
2021-07-01 10:28:10 +0200

    menu link with active state

---
+++ src/components/links.tsx
@@ -0,0 +1,41 @@
+import {
+    NavLink as RouterLink,
+    NavLinkProps as RouterLinkProps,
+    useRouteMatch
+} from 'react-router-dom';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Omit } from '@material-ui/types';
+import React from 'react';
+
+interface ListItemLinkProps {
+    icon?: React.ReactElement;
+    primary: string;
+    to: string;
+}
+
+export const ListItemLink = (props: ListItemLinkProps) => {
+    const { icon, primary, to } = props;
+    let match = useRouteMatch(to);
+
+    const renderLink = React.useMemo(
+        () =>
+            React.forwardRef<any, Omit<RouterLinkProps, 'to'>>(
+                (itemProps, ref) => (
+                    <RouterLink to={to} ref={ref} {...itemProps} />
+                )
+            ),
+        [to]
+    );
+
+    return (
+        <li>
+            <ListItem selected={match !== null} button component={renderLink}>
+                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+                <ListItemText primary={primary} />
+            </ListItem>
+        </li>
+    );
+};
--- src/layout/main/rightDrawer.tsx
+++ src/layout/main/rightDrawer.tsx
@@ -1,38 +1,45 @@
-import CloseIcon from "@material-ui/icons/Close";
-import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import IconButton from "@material-ui/core/IconButton";
-import React from "react";
-import { useMainStyles } from "../../styles/layout/main";
+import CloseIcon from '@material-ui/icons/Close';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import { ListItemLink } from '../../components/links';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import { useMainStyles } from '../../styles/layout/main';
 
 interface RightDrawerProps {
-  isOpen: boolean;
-  toggle: () => void;
+    isOpen: boolean;
+    toggle: () => void;
 }
 
 export const RightDrawer = ({ isOpen, toggle }: RightDrawerProps) => {
-  const classes = useMainStyles();
-  return (
-    <Drawer
-      className={classes.drawer}
-      classes={{
-        paper: classes.drawerPaper,
-      }}
-      anchor="right"
-      open={isOpen}
-      onClose={toggle}
-    >
-      <div className={classes.drawerHeader}>
-        <IconButton
-          color="inherit"
-          aria-label="close drawer"
-          onClick={toggle}
-          className={classes.menuButton}
-        >
-          <CloseIcon />
-        </IconButton>
-      </div>
-      <Divider />
-    </Drawer>
-  );
+    const classes = useMainStyles();
+    return (
+        <Drawer
+            className={classes.drawer}
+            classes={{
+                paper: classes.drawerPaper
+            }}
+            anchor="right"
+            open={isOpen}
+            onClose={toggle}>
+            <div className={classes.drawerHeader}>
+                <IconButton
+                    color="inherit"
+                    aria-label="close drawer"
+                    onClick={toggle}
+                    className={classes.menuButton}>
+                    <CloseIcon />
+                </IconButton>
+            </div>
+            <Divider />
+            <List aria-label="main mailbox folders">
+                <ListItemLink
+                    to="/kontroll"
+                    primary="Kontroll"
+                    icon={<LocationCityIcon />}
+                />
+            </List>
+        </Drawer>
+    );
 };

4f90eb11fd4fd80958f87bf4fa67b4b3518c5327
Ole Tjernstad <ott556@gmail.com>
2021-07-01 21:41:12 +0200

    load klients into kontroll context

---
+++ src/data/kontroll/contracts.ts
@@ -0,0 +1,21 @@
+import { Klient } from '../../contracts/kontrollApi';
+
+export interface kontrollState {
+    klienter: Array<Klient>;
+}
+
+export enum ActionType {
+    setKlienter
+}
+
+export interface setKlienter {
+    type: ActionType.setKlienter;
+    payload: Array<Klient>;
+}
+
+export type KontrollActions = setKlienter;
+
+export interface ContextInterface {
+    state: kontrollState;
+    loadKlienter: () => Promise<void>;
+}

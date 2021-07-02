a6e59aa184aff20005dd6febc87cb84131a8dd13
Ole Tjernstad <ott556@gmail.com>
2021-07-02 22:38:53 +0200

    added columns

--- src/components/kontroll.tsx
+++ src/components/kontroll.tsx
@@ -14,15 +14,38 @@ interface KontrollTableProps {
 export const KontrollTable = ({ kontroller }: KontrollTableProps) => {
     const columns: GridColDef[] = [
         {
-            field: 'name',
-            headerName: 'Kontroll'
-        },
-        {
             field: 'klient',
             headerName: 'Klient',
             sortable: false,
-            width: 160,
-            valueGetter: (params: GridValueGetterParams) => params.row || ''
+            flex: 1,
+            valueGetter: (params: GridValueGetterParams) =>
+                params.row.Objekt.klient.name || ''
+        },
+        {
+            field: 'object',
+            headerName: 'Lokasjon',
+            sortable: false,
+            flex: 1,
+            valueGetter: (params: GridValueGetterParams) =>
+                params.row.Objekt.name || ''
+        },
+        {
+            field: 'name',
+            headerName: 'Kontroll',
+            flex: 1
+        },
+        {
+            field: 'user',
+            headerName: 'Utførende',
+            sortable: false,
+            flex: 1,
+            valueGetter: (params: GridValueGetterParams) =>
+                params.row.user.id || ''
+        },
+        {
+            field: 'kommentar',
+            headerName: 'Kommentar',
+            flex: 1
         }
     ];
 

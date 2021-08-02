44644a9c8a67101c8eb0ee3b69e12dfbe05b077b
Ole Tjernstad <ott556@gmail.com>
2021-08-02 22:44:42 +0200

    report properties form kontrollør

--- src/schema/reportProperties.tsx
+++ src/schema/reportProperties.tsx
@@ -11,11 +11,31 @@ import Grid from '@material-ui/core/Grid';
 import { LoadingButton } from '../components/button';
 import Select from 'react-select';
 import { TextField } from '../components/input';
+import Typography from '@material-ui/core/Typography';
 import { User } from '../contracts/userApi';
+import { makeStyles } from '@material-ui/core/styles';
 import { useEffectOnce } from '../hooks/useEffectOnce';
 import { useState } from 'react';
 import { useUser } from '../data/user';
 
+interface Option {
+    value: User;
+    label: string;
+}
+interface FormValues {
+    rapportUser: Option | null;
+    adresse: string;
+    kontaktEpost: string;
+    kontaktTelefon: string;
+    kontaktperson: string;
+    kontrollerEpost: string;
+    kontrollerTelefon: string;
+    kontrollsted: string;
+    oppdragsgiver: string;
+    postnr: string;
+    poststed: string;
+    sertifikater: [];
+}
 interface ReportPropertiesSchemaProps {
     onSubmit: () => void;
     kontroll: ReportKontroll;
@@ -24,15 +44,12 @@ export const ReportPropertiesSchema = ({
     onSubmit,
     kontroll
 }: ReportPropertiesSchemaProps): JSX.Element => {
+    const classes = useStyles();
     const {
         state: { users },
         loadUsers
     } = useUser();
 
-    interface Option {
-        value: User;
-        label: string;
-    }
     const [userOptions, setUserOptions] = useState<Array<Option>>();
 
     useEffect(() => {
@@ -45,6 +62,10 @@ export const ReportPropertiesSchema = ({
         loadUsers();
     });
     const initialData = useMemo(() => {
+        const user =
+            kontroll !== undefined
+                ? userOptions?.find((u) => u.value.id === kontroll.user.id)
+                : null;
         if (kontroll.rapportEgenskaper !== null) {
             return kontroll.rapportEgenskaper;
         }
@@ -54,111 +75,205 @@ export const ReportPropertiesSchema = ({
             kontaktEpost: '',
             kontaktTelefon: '',
             kontaktperson: '',
-            kontrollerEpost: '',
-            kontrollerTelefon: '',
+            kontrollerEpost: user !== undefined ? user?.value.email : '',
+            kontrollerTelefon: user !== undefined ? user?.value.phone : '',
             kontrollsted: '',
             oppdragsgiver: '',
             postnr: '',
             poststed: '',
-            rapportUser: '',
+            rapportUser: user !== undefined ? user : null,
             sertifikater: ''
         };
-    }, [kontroll]);
-
-    return (
-        <Formik
-            initialValues={initialData}
-            validationSchema={Yup.object({})}
-            onSubmit={(values, { setSubmitting }) => {
-                onSubmit();
-            }}>
-            {({ isSubmitting, setFieldValue, values, errors }) => {
-                return (
-                    <Form>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    id="oppdragsgiver"
-                                    label="Oppdragsgiver"
-                                    name="oppdragsgiver"
-                                />
+    }, [kontroll, userOptions]);
+    if (userOptions !== undefined) {
+        return (
+            <Formik
+                initialValues={initialData}
+                validationSchema={Yup.object({})}
+                onSubmit={(values, { setSubmitting }) => {
+                    onSubmit();
+                }}>
+                {({ isSubmitting, setFieldValue, values, errors }) => {
+                    return (
+                        <Form>
+                            <Grid
+                                container
+                                spacing={3}
+                                className={classes.formGroup}>
+                                <Typography
+                                    variant="h3"
+                                    className={classes.blockTitle}>
+                                    Informasjon om inspeksjonsstedet
+                                </Typography>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="oppdragsgiver"
+                                        label="Oppdragsgiver"
+                                        name="oppdragsgiver"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="kontrollsted"
+                                        label="Kontrollsted"
+                                        name="kontrollsted"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="adresse"
+                                        label="Adresse"
+                                        name="adresse"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="postnr"
+                                        label="Postnr"
+                                        name="postnr"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="poststed"
+                                        label="Poststed"
+                                        name="poststed"
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    id="kontrollsted"
-                                    label="Kontrollsted"
-                                    name="kontrollsted"
-                                />
+                            {/* Kontrollert av under */}
+                            <Grid
+                                container
+                                spacing={3}
+                                className={classes.formGroup}>
+                                <Typography
+                                    variant="h3"
+                                    className={classes.blockTitle}>
+                                    Kontrollert av
+                                </Typography>
+                                <Grid item xs={12}>
+                                    {users && (
+                                        <div>
+                                            <label htmlFor="rapportUser-select">
+                                                Kontrollør
+                                            </label>
+                                            <Select
+                                                inputId="rapportUser-select"
+                                                className="basic-single"
+                                                classNamePrefix="select"
+                                                isSearchable
+                                                onChange={(selected) => {
+                                                    if (selected !== null) {
+                                                        setFieldValue(
+                                                            'rapportUser',
+                                                            selected
+                                                        );
+                                                    }
+                                                }}
+                                                value={values.rapportUser}
+                                                name="rapportUser"
+                                                options={userOptions}
+                                                styles={{
+                                                    menuPortal: (base) => ({
+                                                        ...base,
+                                                        zIndex: 9999
+                                                    })
+                                                }}
+                                                menuPortalTarget={document.body}
+                                            />
+                                        </div>
+                                    )}
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <KontrollerContactField
+                                        userProp="email"
+                                        label="Epost"
+                                        name="kontrollerEpost"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <KontrollerContactField
+                                        userProp="phone"
+                                        label="Telefon"
+                                        name="kontrollerTelefon"
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    id="adresse"
-                                    label="Adresse"
-                                    name="adresse"
-                                />
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <LoadingButton
+                                        isLoading={isSubmitting}
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary">
+                                        Lagre
+                                    </LoadingButton>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    id="postnr"
-                                    label="Postnr"
-                                    name="postnr"
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    id="poststed"
-                                    label="Poststed"
-                                    name="poststed"
-                                />
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <LoadingButton
-                                    isLoading={isSubmitting}
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary">
-                                    Lagre
-                                </LoadingButton>
-                            </Grid>
-                        </Grid>
-                    </Form>
-                );
-            }}
-        </Formik>
-    );
+                        </Form>
+                    );
+                }}
+            </Formik>
+        );
+    }
+    return <div>Laster</div>;
 };
 
-// const EnhetField = () => {
-//     const {
-//         values: { type },
-//         setFieldValue
-//     } = useFormikContext<FormValues>();
+export const useStyles = makeStyles((theme) => ({
+    formGroup: {
+        borderStyle: 'solid',
+        borderColor: theme.palette.primary.main,
+        borderWidth: '1px',
+        borderRadius: 3,
+        marginBottom: 18,
+        marginTop: 2
+    },
+    blockTitle: {
+        fontSize: '1.2rem',
+        padding: 12,
+        paddingBottom: 0
+    }
+}));
 
-//     useEffect(() => {
-//         if (type !== null) {
-//             setFieldValue('enhet', type.value.enhet);
-//         }
-//     }, [setFieldValue, type]);
+interface KontrollerContactFieldProps {
+    userProp: 'email' | 'phone';
+    name: string;
+    label: string;
+}
+const KontrollerContactField = ({
+    name,
+    label,
+    userProp
+}: KontrollerContactFieldProps) => {
+    const {
+        values: { rapportUser },
+        setFieldValue
+    } = useFormikContext<FormValues>();
 
-//     return (
-//         <TextField
-//             variant="outlined"
-//             fullWidth
-//             id="enhet"
-//             label="Enhet"
-//             name="enhet"
-//         />
-//     );
-// };
+    useEffect(() => {
+        if (rapportUser !== null) {
+            setFieldValue(name, rapportUser.value[userProp] ?? '');
+        }
+    }, [setFieldValue, rapportUser, userProp, name]);
+
+    return (
+        <TextField
+            variant="outlined"
+            fullWidth
+            id={name}
+            label={label}
+            name={name}
+        />
+    );
+};

4ded6714809246a80d64b719815abcdc6a1dca6e
Ole Tjernstad <ott556@gmail.com>
2021-08-15 19:35:05 +0200

    space

--- src/components/uploader.tsx
+++ src/components/uploader.tsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core';
 import { useDropzone } from 'react-dropzone';
+
 interface DropZoneProps {
     accept: 'image/png, image/jpeg' | 'image/*';
     setFiles: React.Dispatch<React.SetStateAction<File[]>>;

9f48155000201ccde838e5f2751a02801a24689c
Ole Tjernstad <ott556@gmail.com>
2021-08-24 22:37:34 +0200

    fix issue with not checked checkbox

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -64,7 +64,7 @@ export const Checkbox = ({ label, name }: CheckboxProps) => {
     // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
     // which we can spread on <input>. We can use field meta to show an error
     // message if the field is invalid and it has been touched (i.e. visited)
-    const [field, meta] = useField({ name });
+    const [field, meta] = useField({ name, type: 'checkbox' });
     return (
         <FormControl
             required

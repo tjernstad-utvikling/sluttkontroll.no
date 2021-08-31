67b409d38e92d50596dc40e24eec354870779221
Ole Tjernstad <ott556@gmail.com>
2021-08-31 11:46:12 +0200

    download certificate file

--- src/views/instrument-calibration.tsx
+++ src/views/instrument-calibration.tsx
@@ -1,6 +1,5 @@
 // Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 import {
     CalibrationTable,
@@ -21,7 +20,7 @@ import { InstrumentCalibrationViewParams } from '../contracts/navigation';
 import { TableContainer } from '../tables/tableContainer';
 import { Viewer } from '@react-pdf-viewer/core';
 import { Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { getFilePlugin } from '@react-pdf-viewer/get-file';
 import { useInstrument } from '../data/instrument';
 import { usePageStyles } from '../styles/kontroll/page';
 import { useParams } from 'react-router-dom';
@@ -38,7 +37,15 @@ const InstrumentsView = () => {
     const [objectUrl, setObjectUrl] = useState<string | undefined>(undefined);
     const [openCertificateId, setOpenCertificateId] = useState<number>();
 
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const getFilePluginInstance = getFilePlugin({
+        fileNameGenerator: (file) => {
+            if (instrument !== undefined) {
+                return `${instrument.name}-${instrument.serienr}.pdf`;
+            }
+            return 'sertifikat.pdf';
+        }
+    });
+    const { DownloadButton } = getFilePluginInstance;
 
     const openCertificate = async (calibrationId: number) => {
         if (objectUrl !== undefined) {
@@ -125,10 +132,36 @@ const InstrumentsView = () => {
                     <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
                         {objectUrl !== undefined && (
                             <Grid item xs={12} style={{ height: 1250 }}>
-                                <Viewer
-                                    fileUrl={objectUrl}
-                                    plugins={[defaultLayoutPluginInstance]}
-                                />
+                                <div
+                                    className="rpv-core__viewer"
+                                    style={{
+                                        border: '1px solid rgba(0, 0, 0, 0.3)',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        height: '100%'
+                                    }}>
+                                    <div
+                                        style={{
+                                            alignItems: 'right',
+                                            backgroundColor: '#eeeeee',
+                                            borderBottom:
+                                                '1px solid rgba(0, 0, 0, 0.3)',
+                                            display: 'flex',
+                                            padding: '4px'
+                                        }}>
+                                        <DownloadButton />
+                                    </div>
+                                    <div
+                                        style={{
+                                            flex: 1,
+                                            overflow: 'hidden'
+                                        }}>
+                                        <Viewer
+                                            fileUrl={objectUrl}
+                                            plugins={[getFilePluginInstance]}
+                                        />
+                                    </div>
+                                </div>
                             </Grid>
                         )}
                     </Worker>

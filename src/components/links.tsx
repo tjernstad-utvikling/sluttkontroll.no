0ac7b6f25d5b8845f88defe3b4bbc8533ec29aa4
Ole Tjernstad <ott556@gmail.com>
2021-09-02 21:50:06 +0200

    navigation in left drawer

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -9,16 +9,21 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import { Omit } from '@material-ui/types';
 import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 
 interface ListItemLinkProps {
     icon?: React.ReactElement;
     primary: string;
     to: string;
+    lightText?: boolean;
 }
 
 export const ListItemLink = (props: ListItemLinkProps) => {
     const { icon, primary, to } = props;
     const match = useRouteMatch(to);
+
+    const classes = useStyles();
 
     const renderLink = React.useMemo(
         () =>
@@ -33,9 +38,27 @@ export const ListItemLink = (props: ListItemLinkProps) => {
     return (
         <li>
             <ListItem selected={match !== null} button component={renderLink}>
-                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
-                <ListItemText primary={primary} />
+                {icon ? (
+                    <ListItemIcon
+                        className={clsx({
+                            [classes.icon]: props.lightText
+                        })}>
+                        {icon}
+                    </ListItemIcon>
+                ) : null}
+                <ListItemText
+                    className={clsx({
+                        [classes.icon]: props.lightText
+                    })}
+                    primary={primary}
+                />
             </ListItem>
         </li>
     );
 };
+
+const useStyles = makeStyles({
+    icon: {
+        color: '#fff'
+    }
+});
--- src/layout/admin/leftDrawer.tsx
+++ src/layout/admin/leftDrawer.tsx
@@ -1,5 +1,8 @@
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import GroupIcon from '@material-ui/icons/Group';
+import List from '@material-ui/core/List';
+import { ListItemLink } from '../../components/links';
 import clsx from 'clsx';
 import logo from '../../assets/logoWhite.png';
 import { useMainStyles } from '../../styles/layout/main';
@@ -27,6 +30,14 @@ export const LeftDrawer = ({ isOpen }: LeftDrawerProps) => {
                 <img src={logo} alt="" height={100} />
             </div>
             <Divider />
+            <List aria-label="admin meny">
+                <ListItemLink
+                    to="/admin/users"
+                    primary="Brukere"
+                    icon={<GroupIcon />}
+                    lightText
+                />
+            </List>
         </Drawer>
     );
 };

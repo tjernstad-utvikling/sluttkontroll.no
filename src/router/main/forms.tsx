380bd8aaec74c830ce0691ac3eb339bf39b04c81
Ole Tjernstad <ott556@gmail.com>
2021-09-09 16:34:45 +0200

    Forms view

--- src/router/main/forms.tsx
+++ src/router/main/forms.tsx
@@ -4,10 +4,7 @@ import { Suspense, lazy } from 'react';
 import { InstrumentContextProvider } from '../../data/instrument';
 import { MainLayout } from '../../layout/main';
 
-const InstrumentsView = lazy(() => import('../../views/instrument-dashboard'));
-const InstrumentCalibrationView = lazy(
-    () => import('../../views/instrument-calibration')
-);
+const FormsView = lazy(() => import('../../views/forms-dashboard'));
 
 export const Forms = () => {
     let { path } = useRouteMatch();
@@ -16,11 +13,8 @@ export const Forms = () => {
             <MainLayout module="instrument">
                 <Suspense fallback={<div>Loading...</div>}>
                     <Switch>
-                        <Route path={`${path}/:instrumentId/calibration`}>
-                            <InstrumentCalibrationView />
-                        </Route>
                         <Route exact path={path}>
-                            <InstrumentsView />
+                            <FormsView />
                         </Route>
                     </Switch>
                 </Suspense>
--- src/views/forms-dashboard.tsx
+++ src/views/forms-dashboard.tsx
@@ -1,74 +1,10 @@
-import { Card, CardMenu } from '../components/card';
-import {
-    KontrollTable,
-    defaultColumns,
-    kontrollColumns
-} from '../tables/kontroll';
-import { useEffect, useState } from 'react';
-
+import { Card } from '../components/card';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { Kontroll } from '../contracts/kontrollApi';
-import { KontrollEditModal } from '../modal/kontroll';
-import { TableContainer } from '../tables/tableContainer';
-import { useAuth } from '../hooks/useAuth';
-import { useAvvik } from '../data/avvik';
-import { useClient } from '../data/klient';
-import { useEffectOnce } from '../hooks/useEffectOnce';
-import { useHistory } from 'react-router-dom';
-import { useKontroll } from '../data/kontroll';
-import { useMeasurement } from '../data/measurement';
 import { usePageStyles } from '../styles/kontroll/page';
-import { useUser } from '../data/user';
 
-const KontrollerView = () => {
+const FormsView = () => {
     const classes = usePageStyles();
-
-    const history = useHistory();
-    const { user } = useAuth();
-
-    const [_kontroller, setKontroller] = useState<Array<Kontroll>>([]);
-
-    const {
-        state: { kontroller },
-        loadKontroller,
-        toggleStatusKontroll
-    } = useKontroll();
-    const {
-        state: { klienter }
-    } = useClient();
-    const {
-        loadUsers,
-        state: { users }
-    } = useUser();
-
-    const {
-        state: { avvik }
-    } = useAvvik();
-    const {
-        state: { measurements }
-    } = useMeasurement();
-
-    useEffectOnce(() => {
-        loadKontroller();
-        loadUsers();
-    });
-
-    useEffect(() => {
-        if (kontroller !== undefined) {
-            setKontroller(kontroller.filter((k) => k.user.id === user?.id));
-        }
-    }, [kontroller, user?.id]);
-
-    const [editId, setEditId] = useState<number>();
-
-    const editKontroll = (id: number) => {
-        setEditId(id);
-    };
-
-    const closeEdit = () => {
-        setEditId(undefined);
-    };
 
     return (
         <>
@@ -76,49 +12,14 @@ const KontrollerView = () => {
             <Container maxWidth="lg" className={classes.container}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <Card
-                            title="Dine kontroller"
-                            menu={
-                                <CardMenu
-                                    items={[
-                                        {
-                                            label: 'Ny kontroll',
-                                            action: () =>
-                                                history.push('/kontroll/new')
-                                        }
-                                    ]}
-                                />
-                            }>
-                            {kontroller !== undefined ? (
-                                <TableContainer
-                                    columns={kontrollColumns(
-                                        users ?? [],
-                                        klienter ?? [],
-                                        avvik ?? [],
-                                        measurements ?? [],
-                                        editKontroll,
-                                        toggleStatusKontroll
-                                    )}
-                                    defaultColumns={defaultColumns}
-                                    tableId="kontroller">
-                                    <KontrollTable
-                                        klienter={klienter ?? []}
-                                        avvik={avvik ?? []}
-                                        users={users ?? []}
-                                        measurements={measurements ?? []}
-                                        kontroller={_kontroller}
-                                    />
-                                </TableContainer>
-                            ) : (
-                                <div>Laster kontroller</div>
-                            )}
+                        <Card title="Utfylte skjemaer">
+                            <div />
                         </Card>
                     </Grid>
                 </Grid>
             </Container>
-            <KontrollEditModal editId={editId} close={closeEdit} />
         </>
     );
 };
 
-export default KontrollerView;
+export default FormsView;

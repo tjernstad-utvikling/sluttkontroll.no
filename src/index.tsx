7d6cef1861ccf137aca8f33029db27b794fa393c
Ole <ott556@gmail.com>
2021-10-04 14:14:08 +0200

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import "./index.css";
+import './index.css';
 
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+import * as Sentry from '@sentry/react';
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

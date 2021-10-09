4d60df0f69961b8e8d607f42f6c5f249fd20b2b8
Ole Tjernstad <ott556@gmail.com>
2021-10-09 21:18:49 +0200

    add step divider for template group and fields

--- src/views/admin-settings-forms-templateNew.tsx
+++ src/views/admin-settings-forms-templateNew.tsx
@@ -16,21 +16,36 @@ import {
     ResponderProvided
 } from 'react-beautiful-dnd';
 import { FormsGroup, FormsTemplate } from '../contracts/sjaApi';
-import React, { useState } from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
+import AddLocationIcon from '@material-ui/icons/AddLocation';
+import BrandingWatermarkIcon from '@material-ui/icons/BrandingWatermark';
 import { Card } from '../components/card';
 import Container from '@material-ui/core/Container';
 import { FormsTemplateGroupSchema } from '../schema/formsTemplateGroup';
 import { FormsTemplateSchema } from '../schema/formsTemplate';
 import Grid from '@material-ui/core/Grid';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import React from 'react';
+import Step from '@material-ui/core/Step';
+import StepConnector from '@material-ui/core/StepConnector';
+import { StepIconProps } from '@material-ui/core/StepIcon';
+import StepLabel from '@material-ui/core/StepLabel';
+import Stepper from '@material-ui/core/Stepper';
+import TitleIcon from '@material-ui/icons/Title';
+import clsx from 'clsx';
 import { useForms } from '../data/forms';
 import { usePageStyles } from '../styles/kontroll/page';
+import { useState } from 'react';
 
 const FormsTemplateNewView = () => {
     const classes = usePageStyles();
 
     const [template, setTemplate] = useState<FormsTemplate>();
     const [group, setGroup] = useState<FormsGroup>();
+
+    const [activeStep, setActiveStep] = useState(0);
 
     const {
         state: { groups }
@@ -46,31 +61,63 @@ const FormsTemplateNewView = () => {
     const onSaveGroup = async (title: string, description: string) => {
         return false;
     };
+
+    const formSwitch = () => {
+        switch (activeStep) {
+            case 0:
+                return <FormsTemplateSchema onSubmit={onSave} />;
+            case 1:
+                return (
+                    <>
+                        <FormsTemplateGroupSchema onSubmit={onSaveGroup} />
+                        {groups !== undefined ? (
+                            <GroupTable groups={groups} />
+                        ) : (
+                            <p>Ingen grupper registrert</p>
+                        )}
+                    </>
+                );
+            case 2:
+                return <div />;
+        }
+    };
+
     return (
-        <>
+        <div>
             <div className={classes.appBarSpacer} />
             <Container maxWidth="lg" className={classes.container}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <Card title="Ny skjema mal">
-                            <FormsTemplateSchema onSubmit={onSave} />
-                        </Card>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Card title="Grupper">
-                            <FormsTemplateGroupSchema onSubmit={onSaveGroup} />
-                            {groups !== undefined ? (
-                                <GroupTable groups={groups} />
-                            ) : (
-                                <p>Ingen grupper registrert</p>
-                            )}
+                        <Card title="Ny mal">
+                            <Stepper
+                                alternativeLabel
+                                activeStep={activeStep}
+                                connector={<ColorlibConnector />}>
+                                <Step>
+                                    <StepLabel
+                                        StepIconComponent={ColorlibStepIcon}>
+                                        Mal
+                                    </StepLabel>
+                                </Step>
+                                <Step>
+                                    <StepLabel
+                                        StepIconComponent={ColorlibStepIcon}>
+                                        Grupper
+                                    </StepLabel>
+                                </Step>
+                                <Step>
+                                    <StepLabel
+                                        StepIconComponent={ColorlibStepIcon}>
+                                        Felter
+                                    </StepLabel>
+                                </Step>
+                            </Stepper>
+                            {formSwitch()}
                         </Card>
                     </Grid>
                 </Grid>
             </Container>
-        </>
+        </div>
     );
 };
 
@@ -157,3 +204,71 @@ const DroppableComponent =
             </DragDropContext>
         );
     };
+
+const ColorlibConnector = withStyles({
+    alternativeLabel: {
+        top: 22
+    },
+    active: {
+        '& $line': {
+            backgroundImage:
+                'linear-gradient(97deg, rgba(26,77,39,1) 39%, rgba(51,153,77,1) 100%);'
+        }
+    },
+    completed: {
+        '& $line': {
+            backgroundImage:
+                'linear-gradient(97deg, rgba(26,77,39,1) 39%, rgba(51,153,77,1) 100%)'
+        }
+    },
+    line: {
+        height: 3,
+        border: 0,
+        backgroundColor: '#eaeaf0',
+        borderRadius: 1
+    }
+})(StepConnector);
+
+const useColorlibStepIconStyles = makeStyles({
+    root: {
+        backgroundColor: '#ccc',
+        zIndex: 1,
+        color: '#fff',
+        width: 50,
+        height: 50,
+        display: 'flex',
+        borderRadius: '50%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    active: {
+        backgroundImage:
+            'linear-gradient(97deg, rgba(26,77,39,1) 39%, rgba(51,153,77,1) 100%);',
+        boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)'
+    },
+    completed: {
+        backgroundImage:
+            'linear-gradient(97deg, rgba(26,77,39,1) 39%, rgba(51,153,77,1) 100%);'
+    }
+});
+
+function ColorlibStepIcon(props: StepIconProps) {
+    const classes = useColorlibStepIconStyles();
+    const { active, completed } = props;
+
+    const icons: { [index: string]: React.ReactElement } = {
+        1: <TitleIcon />,
+        2: <BrandingWatermarkIcon />,
+        3: <PlaylistAddCheckIcon />
+    };
+
+    return (
+        <div
+            className={clsx(classes.root, {
+                [classes.active]: active,
+                [classes.completed]: completed
+            })}>
+            {icons[String(props.icon)]}
+        </div>
+    );
+}

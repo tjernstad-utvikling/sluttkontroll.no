9de259408cf8bdd8a57575323c69a39e973d0ff2
Ole Tjernstad <ott556@gmail.com>
2021-11-11 22:38:37 +0100

    fix problems with info page

--- src/document/modules/components/sertifikat.tsx
+++ src/document/modules/components/sertifikat.tsx
@@ -11,38 +11,46 @@ export const SertifikatBlock = ({ sertifikater }: SertifikatBlockProps) => {
     return (
         <>
             <TableHeader title="Sertifikater" />
-            {sertifikater.map((s) => (
-                <TableRow key={s.id} hasBottomBorder>
-                    <View style={{ flexGrow: 1, flexDirection: 'column' }}>
-                        <View
-                            style={{
-                                flexGrow: 1,
-                                flexDirection: 'row',
-                                backgroundColor: '#e9eef5'
-                            }}>
-                            <Text style={{ width: 220 }}>{s.type.name}</Text>
-                            <Text>{s.number}</Text>
+            {sertifikater.map((s) => {
+                console.log(s);
+                return (
+                    <TableRow key={s.id} hasBottomBorder>
+                        <View style={{ flexGrow: 1, flexDirection: 'column' }}>
+                            <View
+                                style={{
+                                    flexGrow: 1,
+                                    flexDirection: 'row',
+                                    backgroundColor: '#e9eef5'
+                                }}>
+                                <Text style={{ width: 220 }}>
+                                    {s.type.name}
+                                </Text>
+                                <Text>{s.number}</Text>
+                            </View>
+                            <View
+                                style={{
+                                    flexGrow: 1,
+                                    flexDirection: 'row'
+                                }}>
+                                <Text style={{ width: 220 }}>Gyldig til</Text>
+                                <Text>
+                                    {format(new Date(s.validTo), 'dd.MM.yyyy')}
+                                </Text>
+                            </View>
                         </View>
                         <View
                             style={{
-                                flexGrow: 1,
-                                flexDirection: 'row'
+                                width: 60,
+                                padding: 5
                             }}>
-                            <Text style={{ width: 220 }}>Gyldig til</Text>
-                            <Text>
-                                {format(new Date(s.validTo), 'dd.MM.yyyy')}
-                            </Text>
+                            {s.type.logoBase64 !== undefined &&
+                                s.type.logoBase64.length > 0 && (
+                                    <Image src={s.type.logoBase64} />
+                                )}
                         </View>
-                    </View>
-                    <View
-                        style={{
-                            width: 60,
-                            padding: 5
-                        }}>
-                        <Image src={s.type.logoBase64} />
-                    </View>
-                </TableRow>
-            ))}
+                    </TableRow>
+                );
+            })}
         </>
     );
 };

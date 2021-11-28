1b25293228688f59fc3873e69458ae1057dfa1f4
Ole Tjernstad <ott556@gmail.com>
2021-11-28 22:07:58 +0100

    support bold and italic in paragraph

--- src/document/modules/components/box.tsx
+++ src/document/modules/components/box.tsx
@@ -5,7 +5,7 @@ interface InfoBoxProps {
     text: OutputData;
 }
 export const InfoBox = ({ text }: InfoBoxProps) => {
-    const header = (block: OutputBlockData<string, any>): JSX.Element => {
+    function header(block: OutputBlockData<string, any>): JSX.Element {
         switch (block.data.level) {
             case 1:
                 return (
@@ -24,23 +24,63 @@ export const InfoBox = ({ text }: InfoBoxProps) => {
             default:
                 return <Text></Text>;
         }
-    };
+    }
+
+    function paragraph(text: string, id: string | undefined): JSX.Element {
+        const boldOrItalic = /(<(?:b|i)>)([^<]+)<\/(?:b|i)>/gim;
+
+        const output: JSX.Element[] = [];
+        const textStyling: string[] = [];
+
+        for (const splitText of text.split(boldOrItalic)) {
+            if (textStyling[textStyling.length - 1] === 'bold') {
+                output.push(
+                    <Text
+                        key={splitText}
+                        style={{
+                            fontWeight: 'bold'
+                        }}>
+                        {splitText}
+                    </Text>
+                );
+            } else if (textStyling[textStyling.length - 1] === 'italic') {
+                output.push(
+                    <Text
+                        key={splitText}
+                        style={{
+                            fontStyle: 'italic'
+                        }}>
+                        {splitText}
+                    </Text>
+                );
+            } else {
+                if (splitText !== '<b>' && splitText !== '<i>')
+                    output.push(<Text key={splitText}>{splitText}</Text>);
+            }
+            if (splitText === '<b>') textStyling.push('bold');
+            else if (splitText === '<i>') textStyling.push('italic');
+            else textStyling.push('');
+        }
+
+        return (
+            <Text
+                style={{
+                    fontSize: 12,
+                    paddingBottom: 10,
+                    fontFamily: 'Arial'
+                }}
+                key={id}>
+                {output}
+            </Text>
+        );
+    }
+
     const block = (block: OutputBlockData<string, any>): JSX.Element => {
         switch (block.type) {
             case 'header':
                 return header(block);
             case 'paragraph':
-                return (
-                    <Text
-                        style={{
-                            fontSize: 12,
-                            paddingBottom: 10,
-                            fontFamily: 'Arial'
-                        }}
-                        key={block.id}>
-                        {block.data.text}
-                    </Text>
-                );
+                return paragraph(block.data.text, block.id);
             default:
                 return <Text></Text>;
         }

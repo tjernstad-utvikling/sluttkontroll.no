32eaa0188d1e1eb0bbb40a0d55cf752e6159fc78
Ole Tjernstad <ott556@gmail.com>
2021-11-28 22:43:00 +0100

    fix errors

--- src/document/modules/components/text.tsx
+++ src/document/modules/components/text.tsx
@@ -1,32 +1,32 @@
 import { StyleSheet, Text } from '@react-pdf/renderer';
 
-export const Paragraph = (children?: React.ReactNode) => {
+export const Paragraph = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={styles.p}>{children}</Text>;
 };
-export const BoldText = (children?: React.ReactNode) => {
+export const BoldText = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={styles.bold}>{children}</Text>;
 };
-export const ItalicText = (children?: React.ReactNode) => {
+export const ItalicText = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={styles.italic}>{children}</Text>;
 };
-export const Header1 = (children?: React.ReactNode) => {
+export const Header1 = ({ children }: { children?: React.ReactNode }) => {
     return (
         <Text style={{ fontSize: 24, textAlign: 'center' }}>{children}</Text>
     );
 };
-export const Header2 = (children?: React.ReactNode) => {
+export const Header2 = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={{ fontSize: 22 }}>{children}</Text>;
 };
-export const Header3 = (children?: React.ReactNode) => {
+export const Header3 = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={{ fontSize: 18 }}>{children}</Text>;
 };
-export const Header4 = (children?: React.ReactNode) => {
+export const Header4 = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={{ fontSize: 16 }}>{children}</Text>;
 };
-export const Header5 = (children?: React.ReactNode) => {
+export const Header5 = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={{ fontSize: 12 }}>{children}</Text>;
 };
-export const Header6 = (children?: React.ReactNode) => {
+export const Header6 = ({ children }: { children?: React.ReactNode }) => {
     return <Text style={{ fontSize: 10 }}>{children}</Text>;
 };
 
@@ -40,6 +40,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     italic: {
-        fontWeight: 'bold'
+        fontStyle: 'italic'
     }
 });

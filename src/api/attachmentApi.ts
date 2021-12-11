e80206b32601c4d140d0ae480b26cb0c89431793
Ole Tjernstad <ott556@gmail.com>
2021-12-11 23:09:03 +0100

    attachment api

---
+++ src/api/attachmentApi.ts
@@ -0,0 +1,35 @@
+import { Attachment } from '../contracts/attachmentApi';
+import { errorHandler } from '../tools/errorHandler';
+import sluttkontrollApi from './sluttkontroll';
+
+export const uploadAttachment = async (
+    kontrollId: number,
+    attachment: File,
+    name: string,
+    description: string
+): Promise<{ status: number; attachment?: Attachment; message?: string }> => {
+    try {
+        const formData = new FormData();
+
+        formData.append('attachment', attachment);
+        formData.append('data', JSON.stringify({ name, description }));
+
+        const { status, data } = await sluttkontrollApi.post(
+            `/attachment/add/${kontrollId}`,
+            formData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        );
+
+        return { status, ...data };
+    } catch (error: any) {
+        if (error.response.status === 400) {
+            return { status: 400, message: error.response.data.message };
+        }
+        errorHandler(error);
+        throw new Error(error);
+    }
+};

00a4355e6a92390ed77a5adac52a51bcf2a25313
Ole Tjernstad <ott556@gmail.com>
2021-12-15 22:26:09 +0100

    add modal for locationImage

---
+++ src/modal/locationImage.tsx
@@ -0,0 +1,81 @@
+import { Avvik } from '../contracts/avvikApi';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { DropZone } from '../components/uploader';
+import { NewImageCard } from '../components/avvik';
+import { Theme } from '@mui/material';
+import createStyles from '@mui/styles/createStyles';
+import makeStyles from '@mui/styles/makeStyles';
+import { useAvvik } from '../data/avvik';
+import { useState } from 'react';
+
+interface LocationImageModalProps {
+    open: boolean;
+    close: () => void;
+    avvik: Avvik;
+}
+export function LocationImageModal({
+    open,
+    close,
+    avvik
+}: LocationImageModalProps) {
+    const classes = useStyles();
+    const [images, setImages] = useState<File[]>([]);
+
+    const { addAvvikImages } = useAvvik();
+
+    const saveNewImages = async () => {
+        if (await addAvvikImages(avvik, images)) {
+            setImages([]);
+            close();
+        }
+    };
+
+    return (
+        <Dialog
+            open={open}
+            onClose={close}
+            aria-labelledby="add-Picture-Dialog">
+            <DialogTitle id="add-Picture-Dialog">Legg til bilder</DialogTitle>
+            <DialogContent>
+                <DropZone
+                    multiple
+                    accept="image/png, image/jpeg"
+                    setFiles={setImages}
+                    files={images}>
+                    <div className={classes.imageContainer}>
+                        {images.map((img) => (
+                            <NewImageCard
+                                key={img.name}
+                                file={img}
+                                setFiles={setImages}
+                            />
+                        ))}
+                    </div>
+                </DropZone>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={close} color="error">
+                    Avbryt
+                </Button>
+                <Button onClick={saveNewImages} color="primary">
+                    Lagre
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        imageContainer: {
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            alignItems: 'flex-start'
+        }
+    })
+);

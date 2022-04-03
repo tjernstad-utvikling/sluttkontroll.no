2bd319af45c9e76371816c603174472eb4852fbb
Ole Tjernstad <ott556@gmail.com>
2022-04-03 12:22:05 +0200

    useoøp¨'1Kontr.oller

--- src/api/hooks/useKontroll.tsx
+++ src/api/hooks/useKontroll.tsx
@@ -4,13 +4,49 @@ import sluttkontrollApi from '../sluttkontroll';
 import { useQuery } from 'react-query';
 
 export function useExternalKontroller() {
-    return useQuery('externalKontroll', () => getKontrollerForExternal());
+    return useQuery('externalKontroll', async () => {
+        const { data } = await sluttkontrollApi.get<{
+            kontroller: Kontroll[];
+            skjemaer: Skjema[];
+            checklists: Checklist[];
+        }>(`/kontroll/access-by-external`);
+        return data;
+    });
 }
-const getKontrollerForExternal = async () => {
-    const { data } = await sluttkontrollApi.get<{
-        kontroller: Kontroll[];
-        skjemaer: Skjema[];
-        checklists: Checklist[];
-    }>(`/kontroll/access-by-external`);
-    return data;
-};
+
+export function useKontroller({
+    includeDone,
+    clientId,
+    locationId
+}: {
+    includeDone: boolean;
+    clientId?: number;
+    locationId?: number;
+}) {
+    return useQuery(
+        [
+            'kontroll',
+            includeDone ? 'all' : 'open',
+            ...(locationId
+                ? ['location', locationId]
+                : clientId
+                ? ['client', clientId]
+                : [])
+        ],
+        async () => {
+            const { data } = await sluttkontrollApi.get<{
+                kontroller: Kontroll[];
+            }>(`/kontroll`, {
+                params: {
+                    ...(includeDone ? { all: true } : {}),
+                    ...(locationId
+                        ? { locationId }
+                        : clientId
+                        ? { clientId }
+                        : {})
+                }
+            });
+            return data;
+        }
+    );
+}

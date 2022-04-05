1dcb1e69c019cb596b692f052144b3879a7b6a2a
Ole Tjernstad <ott556@gmail.com>
2022-04-05 12:35:31 +0200

    get calibration by filename not id

--- src/api/instrumentApi.ts
+++ src/api/instrumentApi.ts
@@ -1,196 +1,196 @@
-import { Instrument, Kalibrering } from '../contracts/instrumentApi';
+import { Instrument, Kalibrering } from "../contracts/instrumentApi";
 
-import { User } from '../contracts/userApi';
-import { errorHandler } from '../tools/errorHandler';
-import sluttkontrollApi from './sluttkontroll';
+import { User } from "../contracts/userApi";
+import { errorHandler } from "../tools/errorHandler";
+import sluttkontrollApi from "./sluttkontroll";
 
 interface InstrumentResponse {
-    status: number;
-    instruments: Instrument[];
+  status: number;
+  instruments: Instrument[];
 }
 
 export const getInstruments = async (): Promise<InstrumentResponse> => {
-    try {
-        const { status, data } = await sluttkontrollApi.get('/instrument/');
-        return { status, ...data };
-    } catch (error: any) {
-        throw new Error('Error Instrument API Get instruments');
-    }
+  try {
+    const { status, data } = await sluttkontrollApi.get("/instrument/");
+    return { status, ...data };
+  } catch (error: any) {
+    throw new Error("Error Instrument API Get instruments");
+  }
 };
 interface InstrumentStatusResponse {
-    status: number;
-    dispInstruments?: Instrument[];
-    ownedInstruments?: Instrument[];
+  status: number;
+  dispInstruments?: Instrument[];
+  ownedInstruments?: Instrument[];
 }
 export const getInstrumentStatusCurrentUser =
-    async (): Promise<InstrumentStatusResponse> => {
-        try {
-            const { status, data } = await sluttkontrollApi.get(
-                '/instrument/calibration/status'
-            );
-            return { status, ...data };
-        } catch (error: any) {
-            return { status: error.response.status };
-        }
-    };
+  async (): Promise<InstrumentStatusResponse> => {
+    try {
+      const { status, data } = await sluttkontrollApi.get(
+        "/instrument/calibration/status"
+      );
+      return { status, ...data };
+    } catch (error: any) {
+      return { status: error.response.status };
+    }
+  };
 
 export const newInstrument = async (instrument: {
-    name: string;
-    serienr: string;
-    user: User | null;
-    toCalibrate: boolean;
-    calibrationInterval: number;
+  name: string;
+  serienr: string;
+  user: User | null;
+  toCalibrate: boolean;
+  calibrationInterval: number;
 }): Promise<{ status: number; instrument?: Instrument; message?: string }> => {
-    try {
-        const user = instrument.user ? { id: instrument.user.id } : null;
-        const { status, data } = await sluttkontrollApi.post('/instrument/', {
-            ...instrument,
-            user
-        });
-        return { status, ...data };
-    } catch (error: any) {
-        if (error.response.status === 400) {
-            return { status: 400, message: error.response.data.message };
-        }
-        throw new Error(error);
+  try {
+    const user = instrument.user ? { id: instrument.user.id } : null;
+    const { status, data } = await sluttkontrollApi.post("/instrument/", {
+      ...instrument,
+      user,
+    });
+    return { status, ...data };
+  } catch (error: any) {
+    if (error.response.status === 400) {
+      return { status: 400, message: error.response.data.message };
     }
+    throw new Error(error);
+  }
 };
 
 export const editInstrument = async (
-    instrument: Instrument
+  instrument: Instrument
 ): Promise<{ status: number; message: string }> => {
-    try {
-        const user = instrument.user ? { id: instrument.user.id } : null;
-        const disponent = instrument.disponent
-            ? { id: instrument.disponent.id }
-            : null;
+  try {
+    const user = instrument.user ? { id: instrument.user.id } : null;
+    const disponent = instrument.disponent
+      ? { id: instrument.disponent.id }
+      : null;
 
-        const { status, data } = await sluttkontrollApi.put(
-            `/instrument/${instrument.id}`,
-            { ...instrument, user, disponent }
-        );
-        return { status, ...data };
-    } catch (error: any) {
-        if (error.response.status === 400) {
-            return { status: 400, message: error.response.data.message };
-        }
-        throw new Error(error);
+    const { status, data } = await sluttkontrollApi.put(
+      `/instrument/${instrument.id}`,
+      { ...instrument, user, disponent }
+    );
+    return { status, ...data };
+  } catch (error: any) {
+    if (error.response.status === 400) {
+      return { status: 400, message: error.response.data.message };
     }
+    throw new Error(error);
+  }
 };
 
 export const setDisponent = async (
-    instrumentId: number,
-    userId: number
+  instrumentId: number,
+  userId: number
 ): Promise<{ status: number }> => {
-    try {
-        const { status } = await sluttkontrollApi.put(
-            `/instrument/disponent/${instrumentId}/${userId}`
-        );
-        return { status };
-    } catch (error: any) {
-        throw new Error('Error Instrument API set Disponent');
-    }
+  try {
+    const { status } = await sluttkontrollApi.put(
+      `/instrument/disponent/${instrumentId}/${userId}`
+    );
+    return { status };
+  } catch (error: any) {
+    throw new Error("Error Instrument API set Disponent");
+  }
 };
 
 export const removeDisponent = async (
-    instrumentId: number
+  instrumentId: number
 ): Promise<{ status: number }> => {
-    try {
-        const { status } = await sluttkontrollApi.put(
-            `/instrument/remove-disponent/${instrumentId}`
-        );
-        return { status };
-    } catch (error: any) {
-        throw new Error('Error Instrument API set Disponent');
-    }
+  try {
+    const { status } = await sluttkontrollApi.put(
+      `/instrument/remove-disponent/${instrumentId}`
+    );
+    return { status };
+  } catch (error: any) {
+    throw new Error("Error Instrument API set Disponent");
+  }
 };
 
 export const addCalibrationFile = async (
-    kalibreringId: number,
-    sertifikatFile: File
+  kalibreringId: number,
+  sertifikatFile: File
 ): Promise<{ status: number; message?: string }> => {
-    try {
-        const formData = new FormData();
+  try {
+    const formData = new FormData();
 
-        formData.append('sertifikatFile', sertifikatFile);
+    formData.append("sertifikatFile", sertifikatFile);
 
-        const { status, data } = await sluttkontrollApi.post(
-            `/instrument/kalibrering/upload-sertifikat/${kalibreringId}`,
-            formData,
-            {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            }
-        );
+    const { status, data } = await sluttkontrollApi.post(
+      `/instrument/kalibrering/upload-sertifikat/${kalibreringId}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
 
-        return { status, ...data };
-    } catch (error: any) {
-        if (error.response.status === 400) {
-            return { status: 400, message: error.response.data.message };
-        }
-        errorHandler(error);
-        throw new Error(error);
+    return { status, ...data };
+  } catch (error: any) {
+    if (error.response.status === 400) {
+      return { status: 400, message: error.response.data.message };
     }
+    errorHandler(error);
+    throw new Error(error);
+  }
 };
 
 export const addCalibration = async (
-    instrumentId: number,
-    kalibrertDate: string
+  instrumentId: number,
+  kalibrertDate: string
 ): Promise<{ status: number; instrument?: Instrument; message?: string }> => {
-    try {
-        const { status, data } = await sluttkontrollApi.post(
-            `/instrument/kalibrering/${instrumentId}`,
-            { kalibrertDate }
-        );
-        return { status, ...data };
-    } catch (error: any) {
-        if (error.response.status === 400) {
-            return { status: 400, message: error.response.data.message };
-        }
-        throw new Error(error);
+  try {
+    const { status, data } = await sluttkontrollApi.post(
+      `/instrument/kalibrering/${instrumentId}`,
+      { kalibrertDate }
+    );
+    return { status, ...data };
+  } catch (error: any) {
+    if (error.response.status === 400) {
+      return { status: 400, message: error.response.data.message };
     }
+    throw new Error(error);
+  }
 };
 
 export const getCalibrationsByInstrument = async (
-    instrumentId: number
+  instrumentId: number
 ): Promise<{
-    status: number;
-    calibrations?: Kalibrering[];
-    message?: string;
+  status: number;
+  calibrations?: Kalibrering[];
+  message?: string;
 }> => {
-    try {
-        const { status, data } = await sluttkontrollApi.get(
-            `/instrument/kalibrering/${instrumentId}`
-        );
-        return { status, ...data };
-    } catch (error: any) {
-        if (error.response.status === 400) {
-            return { status: 400, message: error.response.data.message };
-        }
-        throw new Error(error);
+  try {
+    const { status, data } = await sluttkontrollApi.get(
+      `/instrument/kalibrering/${instrumentId}`
+    );
+    return { status, ...data };
+  } catch (error: any) {
+    if (error.response.status === 400) {
+      return { status: 400, message: error.response.data.message };
     }
+    throw new Error(error);
+  }
 };
 
 export const getCalibrationCertificate = async (
-    calibrationId: number
+  calibration: Kalibrering
 ): Promise<{
-    status: number;
-    data?: Blob;
-    message?: string;
+  status: number;
+  data?: Blob;
+  message?: string;
 }> => {
-    try {
-        const { status, data } = await sluttkontrollApi.get(
-            `/instrument/kalibrering-sertifikat/${calibrationId}`,
-            {
-                responseType: 'blob'
-            }
-        );
-        return { status, data };
-    } catch (error: any) {
-        if (error.response.status === 404) {
-            return { status: 404, message: error.response.data.message };
-        }
-        throw new Error(error);
+  try {
+    const { status, data } = await sluttkontrollApi.get(
+      `/instrument/kalibrering-sertifikat/${calibration.kalibreringSertifikat.fileName}`,
+      {
+        responseType: "blob",
+      }
+    );
+    return { status, data };
+  } catch (error: any) {
+    if (error.response.status === 404) {
+      return { status: 404, message: error.response.data.message };
     }
+    throw new Error(error);
+  }
 };
--- src/views/instrument-calibration.tsx
+++ src/views/instrument-calibration.tsx
@@ -1,139 +1,136 @@
 // Import the styles
 
-import '@react-pdf-viewer/core/lib/styles/index.css';
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 import {
-    CalibrationTable,
-    calibrationColumns,
-    defaultColumns
-} from '../tables/calibration';
-import { Card, CardContent } from '../components/card';
-import { Instrument, Kalibrering } from '../contracts/instrumentApi';
+  CalibrationTable,
+  calibrationColumns,
+  defaultColumns,
+} from "../tables/calibration";
+import { Card, CardContent } from "../components/card";
+import { Instrument, Kalibrering } from "../contracts/instrumentApi";
 import {
-    getCalibrationCertificate,
-    getCalibrationsByInstrument
-} from '../api/instrumentApi';
-import { useEffect, useState } from 'react';
+  getCalibrationCertificate,
+  getCalibrationsByInstrument,
+} from "../api/instrumentApi";
+import { useEffect, useState } from "react";
 
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import { InstrumentCalibrationViewParams } from '../contracts/navigation';
-import { PdfViewer } from '../components/viewer';
-import { TableContainer } from '../tables/tableContainer';
-import { errorHandler } from '../tools/errorHandler';
-import { useInstrument } from '../data/instrument';
-import { usePageStyles } from '../styles/kontroll/page';
-import { useParams } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { InstrumentCalibrationViewParams } from "../contracts/navigation";
+import { PdfViewer } from "../components/viewer";
+import { TableContainer } from "../tables/tableContainer";
+import { errorHandler } from "../tools/errorHandler";
+import { useInstrument } from "../data/instrument";
+import { usePageStyles } from "../styles/kontroll/page";
+import { useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const InstrumentsView = () => {
-    const { classes } = usePageStyles();
-    const { instrumentId } = useParams<InstrumentCalibrationViewParams>();
+  const { classes } = usePageStyles();
+  const { instrumentId } = useParams<InstrumentCalibrationViewParams>();
 
-    const [_calibrations, setCalibrations] = useState<Kalibrering[]>();
-    const [loadedInstrumentId, setLoadedInstrumentId] = useState<number>();
-    const [instrument, setInstrument] = useState<Instrument>();
+  const [_calibrations, setCalibrations] = useState<Kalibrering[]>();
+  const [loadedInstrumentId, setLoadedInstrumentId] = useState<number>();
+  const [instrument, setInstrument] = useState<Instrument>();
 
-    const [objectUrl, setObjectUrl] = useState<string | undefined>(undefined);
-    const [openCertificateId, setOpenCertificateId] = useState<number>();
+  const [objectUrl, setObjectUrl] = useState<string | undefined>(undefined);
+  const [openCertificateId, setOpenCertificateId] = useState<number>();
 
-    const openCertificate = async (calibrationId: number) => {
-        if (objectUrl !== undefined) {
-            URL.revokeObjectURL(objectUrl);
-        }
-        setObjectUrl(undefined);
-        setOpenCertificateId(undefined);
-        try {
-            const response = await getCalibrationCertificate(calibrationId);
+  const openCertificate = async (calibrationId: number) => {
+    if (objectUrl !== undefined) {
+      URL.revokeObjectURL(objectUrl);
+    }
+    setObjectUrl(undefined);
+    setOpenCertificateId(undefined);
+    try {
+      const cal = _calibrations.find((c) => c.id === calibrationId);
+      if (!cal) return;
+      const response = await getCalibrationCertificate(cal);
 
-            if (response.status === 200 && response.data !== undefined) {
-                const blob = new Blob([response.data]);
+      if (response.status === 200 && response.data !== undefined) {
+        const blob = new Blob([response.data]);
 
-                setObjectUrl(URL.createObjectURL(blob));
-                setOpenCertificateId(calibrationId);
-            }
-            if (response.status === 404) {
-                enqueueSnackbar('Kalibreringssertifikat ikke funnet', {
-                    variant: 'warning'
-                });
-            }
-        } catch (error: any) {
-            enqueueSnackbar('Ukjent problem med lasting av sertifikat', {
-                variant: 'error'
-            });
-            errorHandler(error);
-        }
+        setObjectUrl(URL.createObjectURL(blob));
+        setOpenCertificateId(calibrationId);
+      }
+      if (response.status === 404) {
+        enqueueSnackbar("Kalibreringssertifikat ikke funnet", {
+          variant: "warning",
+        });
+      }
+    } catch (error: any) {
+      enqueueSnackbar("Ukjent problem med lasting av sertifikat", {
+        variant: "error",
+      });
+      errorHandler(error);
+    }
+  };
+
+  const {
+    state: { instruments },
+  } = useInstrument();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const get = async () => {
+      setInstrument(instruments?.find((i) => i.id === Number(instrumentId)));
+      const { calibrations, status } = await getCalibrationsByInstrument(
+        Number(instrumentId)
+      );
+      if (status === 200) {
+        setCalibrations(calibrations);
+        setLoadedInstrumentId(Number(instrumentId));
+      } else if (status === 400) {
+        enqueueSnackbar("Ingen kalibreringer funnet", {
+          variant: "warning",
+        });
+      }
     };
+    if (Number(instrumentId) !== loadedInstrumentId) get();
+  }, [enqueueSnackbar, instrumentId, instruments, loadedInstrumentId]);
 
-    const {
-        state: { instruments }
-    } = useInstrument();
+  return (
+    <>
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Card title="Kalibreringer">
+              <CardContent>
+                {_calibrations !== undefined && instrument !== undefined ? (
+                  <TableContainer
+                    columns={calibrationColumns({
+                      openCertificate,
+                      openCertificateId,
+                      instrumentLastCalibration: instrument?.sisteKalibrert,
+                    })}
+                    defaultColumns={defaultColumns}
+                    tableId="calibrations"
+                  >
+                    <CalibrationTable calibrations={_calibrations ?? []} />
+                  </TableContainer>
+                ) : (
+                  <div>Laster kalibreringer</div>
+                )}
+              </CardContent>
+            </Card>
+          </Grid>
 
-    const { enqueueSnackbar } = useSnackbar();
-
-    useEffect(() => {
-        const get = async () => {
-            setInstrument(
-                instruments?.find((i) => i.id === Number(instrumentId))
-            );
-            const { calibrations, status } = await getCalibrationsByInstrument(
-                Number(instrumentId)
-            );
-            if (status === 200) {
-                setCalibrations(calibrations);
-                setLoadedInstrumentId(Number(instrumentId));
-            } else if (status === 400) {
-                enqueueSnackbar('Ingen kalibreringer funnet', {
-                    variant: 'warning'
-                });
-            }
-        };
-        if (Number(instrumentId) !== loadedInstrumentId) get();
-    }, [enqueueSnackbar, instrumentId, instruments, loadedInstrumentId]);
-
-    return (
-        <>
-            <div className={classes.appBarSpacer} />
-            <Container maxWidth="lg" className={classes.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Card title="Kalibreringer">
-                            <CardContent>
-                                {_calibrations !== undefined &&
-                                instrument !== undefined ? (
-                                    <TableContainer
-                                        columns={calibrationColumns({
-                                            openCertificate,
-                                            openCertificateId,
-                                            instrumentLastCalibration:
-                                                instrument?.sisteKalibrert
-                                        })}
-                                        defaultColumns={defaultColumns}
-                                        tableId="calibrations">
-                                        <CalibrationTable
-                                            calibrations={_calibrations ?? []}
-                                        />
-                                    </TableContainer>
-                                ) : (
-                                    <div>Laster kalibreringer</div>
-                                )}
-                            </CardContent>
-                        </Card>
-                    </Grid>
-
-                    <PdfViewer
-                        getFileName={() => {
-                            if (instrument !== undefined) {
-                                return `${instrument.name}-${instrument.serienr}.pdf`;
-                            }
-                            return 'sertifikat.pdf';
-                        }}
-                        fileUrl={objectUrl}
-                    />
-                </Grid>
-            </Container>
-        </>
-    );
+          <PdfViewer
+            getFileName={() => {
+              if (instrument !== undefined) {
+                return `${instrument.name}-${instrument.serienr}.pdf`;
+              }
+              return "sertifikat.pdf";
+            }}
+            fileUrl={objectUrl}
+          />
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default InstrumentsView;
